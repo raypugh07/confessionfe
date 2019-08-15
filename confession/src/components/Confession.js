d72ad96163ae95fcbe9870fc63bba472987e4c7c
Your Name <you@example.com>
2019-08-14 20:23:29 -0500

    fixed form not refreshing and added favicon,confession.js

--- confession/src/components/Confession.js
+++ confession/src/components/Confession.js
@@ -5,7 +5,7 @@ import {
     Grid,
     Header,
     Message,
-    Segment,
+    Segment,Divider
   } from 'semantic-ui-react';
   import './Confession.css'
   import Confessional from './confessional.jpg'
@@ -22,7 +22,7 @@ export default class Confession extends Component {
         }
     }
 
-    inputHandler=e=>{
+   /* inputHandler=e=>{
         this.setState({
             ...this.state,
             task:{
@@ -30,9 +30,9 @@ export default class Confession extends Component {
                 [e.target.name]:e.target.value
             }
         })
-    }
+    }*/
 
-    addHandler=e=>{
+   /* addHandler=e=>{
         e.preventDefault();
         axios
         .post(`https://raypugh07-confessions.herokuapp.com/confessions/confessions`,this.state.task)
@@ -42,35 +42,69 @@ export default class Confession extends Component {
                 ...this.state,
                 task:{
                     confession:""
+                    
                   
                 }
+                
                 
             })
         })
         
         
+    }*/
+
+    //refactored handlers trying to find error preventing form from refreshing...problem is a combination
+    //of Form component from React UI and prevent default on add handler
+
+    inputHandler=event=>{
+        this.setState({[event.target.name]:event.target.value})
+       
     }
+
+    addHandler(event){
+       // event.preventDefault();
+        axios
+        .post(`https://raypugh07-confessions.herokuapp.com/confessions/confessions`,{
+            confession:this.state.confession
+        })
+        .then(res=>{console.log(res)
+        })
+        .catch(err=>console.log(err))
+    }
+
+    
+
     
     render() {
         return (
            <div><Header as="h2" textAlign="center">
                <img className='confessional' alt='confessional' src={Confessional}></img>
           </Header>
-         
+          <Divider hidden />
             <div className='form'> 
-           <Form size="small">
+           <form  onSubmit={e=>this.addHandler(e)}  size="small">
                <Form.Input
                fluid
                icon="chess king"
                iconPosition="left"
                value={this.state.confession}
                onChange={this.inputHandler}
+             
                placeholder='Confess your sins my child...'
                />
-               <Button onClick={e=>this.addHandler(e)} color='blue' fluid size='medium'>You are forgiven.</Button>
-           </Form>
+                <Divider hidden />
+               <Button color='blue' fluid size='medium'>You are forgiven.</Button>
+           </form>
            </div>
            </div>
+          /* <form onSubmit={e=>this.addHandler(e)} > 
+               <input
+               value={this.state.confession}
+               onChange={this.inputHandler}
+               
+               
+               />
+           </form>*/
         )
     }
 }
